e3c8a282f66be12529c21ea29582561671fbee12
Copons <Copons@users.noreply.github.com>
2016-01-20 17:50:18 +0100

    Added Open Sans and Hammer.js, appbar menu style

--- dev/js/gg-ripple.js
+++ dev/js/gg-ripple.js
@@ -1,8 +1,9 @@
-(function ($) {
+(function () {
   'use strict';
 
   Waves.attach('.gg-appbar-icon', ['waves-circle', 'waves-light']);
   Waves.attach('.gg-searchbar-icon', ['waves-circle']);
+  Waves.attach('.gg-appbar-menu a');
   Waves.init();
 
-}(jQuery));
+})();
