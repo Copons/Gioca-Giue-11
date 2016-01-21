20dea708574e0e6f23d03660f9507b5fecd3f225
Copons <Copons@users.noreply.github.com>
2016-01-21 17:48:18 +0100

    Better Material typography; added menu-list style

--- dev/js/gg-ripple.js
+++ dev/js/gg-ripple.js
@@ -3,7 +3,7 @@
 
   Waves.attach('.gg-appbar-icon', ['waves-circle', 'waves-light']);
   Waves.attach('.gg-searchbar-icon', ['waves-circle']);
-  Waves.attach('.gg-appbar-menu a');
+  Waves.attach('.gg-appbar-menu ul li a, .gg-drawer ul li a');
   Waves.init();
 
 })();
