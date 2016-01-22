4c12fadd199d7c896a1231f297be35e5d75a9623
Copons <Copons@users.noreply.github.com>
2016-01-22 15:40:22 +0100

    Material cards, totally WIP

--- dev/js/gg-ripple.js
+++ dev/js/gg-ripple.js
@@ -4,6 +4,7 @@
   Waves.attach('.gg-appbar-icon', ['waves-circle', 'waves-light']);
   Waves.attach('.gg-searchbar-icon', ['waves-circle']);
   Waves.attach('.gg-appbar-menu ul li a, .gg-drawer ul li a');
+  Waves.attach('.gg-button-flat', ['waves-button']);
   Waves.init();
 
 })();
