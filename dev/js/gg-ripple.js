4ada6ad6c799c430a86e5b7646e0c021b33be54c
Copons <Copons@users.noreply.github.com>
2016-01-22 17:37:07 +0100

    Card design follow up

--- dev/js/gg-ripple.js
+++ dev/js/gg-ripple.js
@@ -2,7 +2,7 @@
   'use strict';
 
   Waves.attach('.gg-appbar-icon', ['waves-circle', 'waves-light']);
-  Waves.attach('.gg-searchbar-icon', ['waves-circle']);
+  Waves.attach('.gg-icon-flat, .gg-searchbar-icon', ['waves-circle']);
   Waves.attach('.gg-appbar-menu ul li a, .gg-drawer ul li a');
   Waves.attach('.gg-button-flat', ['waves-button']);
   Waves.init();
