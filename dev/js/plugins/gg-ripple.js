0774fdda893511cacdddd59650221ef8c2b02d85
Copons <Copons@users.noreply.github.com>
2016-01-27 16:23:48 +0100

    More modular cards; Material light text rgba values

--- dev/js/plugins/gg-ripple.js
+++ dev/js/plugins/gg-ripple.js
@@ -3,7 +3,9 @@ import Waves from 'node-waves';
 export default function () {
   Waves.attach('.gg-appbar-icon', ['waves-circle', 'waves-light']);
   Waves.attach('.gg-icon-flat, .gg-searchbar-icon', ['waves-circle']);
+  Waves.attach('.gg-icon-flat-light', ['waves-circle', 'waves-light']);
   Waves.attach('.gg-appbar-menu ul li a, .gg-drawer ul li a');
   Waves.attach('.gg-button-flat', ['waves-button']);
+  Waves.attach('.gg-button-flat-light', ['waves-button', 'waves-light']);
   Waves.init();
 }
