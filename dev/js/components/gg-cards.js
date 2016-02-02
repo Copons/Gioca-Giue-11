35e92750cf91bfc31f5efaaab1d60a6f5ac2eb5e
Copons <copons@users.noreply.github.com>
2016-02-02 20:54:51 +0100

    Fixed card addon z-index bug

--- dev/js/components/gg-cards.js
+++ dev/js/components/gg-cards.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import Masonry from 'masonry-layout';
 
 const ggCardsClass = '.gg-cards';
+const ggCardClass = '.gg-card';
 const $ggCardToggleAddon = $('.gg-card-toggle-addon');
 const ggCardAddonClass = '.gg-card-addon';
 
@@ -38,17 +39,24 @@ export default function () {
   $ggCardToggleAddon.on('click', (event) => {
     event.preventDefault();
     let $toggle = $(event.currentTarget);
+    let $container = $toggle.closest(ggCardClass);
     let $target = $(`${ggCardAddonClass}[data-addon="${$toggle.data('addon')}"]`);
     let innerHeight = 0;
     $target.children().each((index, $item) => {
       innerHeight += $($item).outerHeight(true);
     });
+
     $toggle.ggToggle();
     $target.ggToggle();
+
     if ($target.hasClass('is-visible')) {
+      $container.css({ zIndex : 1 });
       $target.css({ maxHeight : innerHeight });
     }
     else {
+      setTimeout(() => {
+        $container.css({ zIndex : 'auto' });
+      }, 200);
       $target.css({ maxHeight : 0 });
     }
   });
