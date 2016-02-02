c6430073b99c03595f8602662fbf7467e2dfd38f
Copons <Copons@users.noreply.github.com>
2016-02-02 16:24:08 +0100

    Masonry ready for WP Jetpack Infinite Scroll
    + added a tiny header content transition

--- dev/js/components/gg-cards.js
+++ dev/js/components/gg-cards.js
@@ -5,20 +5,34 @@ const ggCardsClass = '.gg-cards';
 const $ggCardToggleAddon = $('.gg-card-toggle-addon');
 const ggCardAddonClass = '.gg-card-addon';
 
+const $body = $(document.body);
 const $masonryContainer = $('#masonry');
 const masonryBlockClass = '#masonry .gg-card';
 
 export default function () {
 
   if ($masonryContainer.length) {
+    let cardsLayout;
     setTimeout(() => {
-      let cardsLayout = new Masonry(document.querySelector(ggCardsClass), {
+      cardsLayout = new Masonry(document.querySelector(ggCardsClass), {
         itemSelector : masonryBlockClass,
         gutter : 16,
         percentPosition : true
       });
       cardsLayout.layout();
+      $(masonryBlockClass).ggToggle('show');
     }, 200);
+
+    // WordPress Jetpack Infinite Scroll
+    let infiniteCount = 0;
+    $body.on('post-load', () => {
+      infiniteCount++;
+      let $infiniteContainer = $('#infinite-view-' + infiniteCount);
+      let $infiniteBlocks = $infiniteContainer.find(ggCardsClass);
+      cardsLayout.appended($infiniteBlocks);
+      $(masonryBlockClass).ggToggle('show');
+    });
+    // CHECK ONCE STAGED!!
   }
 
   $ggCardToggleAddon.on('click', (event) => {
