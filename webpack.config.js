f737907fc43b59eac8e15257669b5e4f329871f1
Copons <Copons@users.noreply.github.com>
2016-02-08 17:00:21 +0100

    Lowered webpack url-loader size limit
    To avoid a multi-MB css file for no good reason, considering no one's
    ever gonna use every font format available.

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ const common = {
       },
       {
         test : /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
-        loader : 'url-loader?limit=100000'
+        loader : 'url-loader?limit=10000'
       }
     ]
   }
